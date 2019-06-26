0793298b9b1ae458dd5c46c17aba70a38e7fed9c
sakitkepala <andikapriyotama@gmail.com>
2019-06-26 12:39:53 +0700

    File init & manifest awal modul todo_app

---
+++ custom-addons/todo_app/__manifest__.py
@@ -0,0 +1,7 @@
+{
+  'name': 'Aplikasi To-Do',
+  'description': 'Modul dari tutorial, untuk tata kelola todo personal',
+  'author': 'Daniel Reis, Andika Priyotama',
+  'depends': ['base'],
+  'application': True,
+}

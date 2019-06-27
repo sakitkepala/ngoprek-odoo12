de08dc2105faab40b4c60eb243b0b698c284ae1b
sakitkepala <andikapriyotama@gmail.com>
2019-06-27 13:43:54 +0700

    Model todo.task

---
+++ custom-addons/todo_app/models/todo_task_model.py
@@ -0,0 +1,17 @@
+from odoo import fields, models
+
+class TodoTask(models.Model):
+  _name = 'todo.task'
+  _description = 'To-Do Task'
+  name = fields.Char('Description', required=True)
+  is_done = fields.Boolean('Done?')
+  active = fields.Boolean('Active?', default=True)
+  user_id = fields.Many2one(
+    'res.users',
+    string='Responsible',
+    default=lambda self: self.env.user
+  )
+  team_ids = fields.Many2many(
+    'res.partner',
+    string='Team'
+  )

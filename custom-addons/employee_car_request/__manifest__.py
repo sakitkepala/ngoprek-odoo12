46a15cfe9eed649a068574a7cb4521bbf3039d3e
sakitkepala <andikapriyotama@gmail.com>
2019-06-30 02:13:21 +0700

    Awal bikin package minimal modul Car Request dan menampilkan menu & viewnya

---
+++ custom-addons/employee_car_request/__manifest__.py
@@ -0,0 +1,10 @@
+{
+  'name': 'Employee Car Request',
+  'description': 'Employee Car Request',
+  'author': 'Andika, M. Magdy',
+  'depends': ['base', 'hr', 'fleet'],
+  'data': [
+    'security/ir.model.access.csv',
+    'views/views.xml',
+  ]
+}
---
+++ custom-addons/employee_car_request/models/models.py
@@ -0,0 +1,11 @@
+from odoo import models, fields, api
+
+class CarRequest(models.Model):
+  _name = 'car.request'
+  _description = 'Car Request'
+
+  name = fields.Char(string='Request', required=True)
+  date_from = fields.Datetime(string='Starting Date', default=fields.Datetime.now())
+  date_to = fields.Datetime(string='End Date', required=False)
+  employee_id = fields.Many2one(comodel_name='hr.employee', string='Employee', required=True)
+  car_id = fields.Many2one(comodel_name='fleet.vehicle', string='Car', required=True)

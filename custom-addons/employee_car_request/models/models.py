99456e3220d5c1b422a77a29e215e784495f5737
sakitkepala <andikapriyotama@gmail.com>
2019-06-30 20:20:18 +0700

    Modif untuk bikin fungsi memproses state request, ditampilkan pada form dengan tombol aksi

--- custom-addons/employee_car_request/models/models.py
+++ custom-addons/employee_car_request/models/models.py
@@ -9,3 +9,30 @@ class CarRequest(models.Model):
   date_to = fields.Datetime(string='End Date', required=False)
   employee_id = fields.Many2one(comodel_name='hr.employee', string='Employee', required=True)
   car_id = fields.Many2one(comodel_name='fleet.vehicle', string='Car', required=True)
+  state = fields.Selection(
+    string='Status',
+    selection=[
+      ('draft','Draft'),
+      ('confirmed','Confirmed'),
+      ('validated','Validated'),
+      ('refused','Refused'),
+      ('approved','Approved'),
+    ],
+    default='draft'
+  )
+
+  @api.multi
+  def confirm_request(self):
+    self.state = 'confirmed'
+
+  @api.multi
+  def validate_request(self):
+    self.state = 'validated'
+
+  @api.multi
+  def refuse_request(self):
+    self.state = 'refused'
+
+  @api.multi
+  def approve_request(self):
+    self.state = 'approved'

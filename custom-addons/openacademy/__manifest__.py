3f4e487e8142f54346db4e75f0bda70cd31ca733
sakitkepala <andikapriyotama@gmail.com>
2019-07-01 16:38:23 +0700

    Bikin modul baru, Open Academy, awal modul modif dari scaffold cmd

---
+++ custom-addons/openacademy/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "openacademy",
+
+    'summary': """
+        Tata kelola training""",
+
+    'description': """
+        Modul Open Academy untuk tata kelola training:
+            - kelas-kelas training
+            - sesi-sesi training
+            - registrasi peserta
+    """,
+
+    'author': "Andika",
+    'website': "https://sakitkepala.github.io",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '12.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+        'views/templates.xml',
+        'views/openacademy.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ custom-addons/openacademy/models/models.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+# class openacademy(models.Model):
+  # _name = 'openacademy.openacademy'
+
+  # name = fields.Char()
+  # value = fields.Integer()
+  # value2 = fields.Float(compute="_value_pc", store=True)
+  # description = fields.Text()
+
+  # @api.depends('value')
+  # def _value_pc(self):
+  #     self.value2 = float(self.value) / 100
+
+class Course(models.Model):
+  _name = 'openacademy.course'
+  _description = "Kelas-Kelas Open Academy"
+
+  name = fields.Char(string='Title', required=True)
+  description = fields.Text()

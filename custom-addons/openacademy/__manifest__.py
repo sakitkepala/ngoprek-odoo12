b40817e46b731c841f8436f52c9c93199b2e2c8b
sakitkepala <andikapriyotama@gmail.com>
2019-07-01 19:02:57 +0700

    View form & search untuk model course

--- custom-addons/openacademy/__manifest__.py
+++ custom-addons/openacademy/__manifest__.py
@@ -26,7 +26,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         # 'views/views.xml',
         'views/templates.xml',
         'views/openacademy.xml',

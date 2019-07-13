7c451d524b50e2ef624447b839b690626645a711
sakitkepala <andikapriyotama@gmail.com>
2019-07-13 09:20:51 +0700

    Ubah nama variabel jadi jamak & loop data untuk session_ids

--- custom-addons/openacademy/models/wizard.py
+++ custom-addons/openacademy/models/wizard.py
@@ -4,14 +4,15 @@ class Wizard(models.TransientModel):
   _name = 'openacademy.wizard'
   _description = "Wizard: Registrasi Kilat Peserta Sesi Kelas"
 
-  def _default_session(self):
-    return self.env['openacademy.session'].browse(self._context.get('active_id'))
+  def _default_sessions(self):
+    return self.env['openacademy.session'].browse(self._context.get('active_ids'))
 
-  session_id = fields.Many2one('openacademy.session',
-    string="Sesi", required=True, default=_default_session)
+  session_ids = fields.Many2many('openacademy.session',
+    string="Sesi", required=True, default=_default_sessions)
   attendee_ids = fields.Many2many('res.partner', string="Peserta")
 
   @api.multi
   def subscribe(self):
-    self.session_id.attendee_ids |= self.attendee_ids
+    for session in self.session_ids:
+      session.attendee_ids |= self.attendee_ids
     return {}

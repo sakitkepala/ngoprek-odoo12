823e13bee2380fc5e4fb3b88dfc5bdcf9fcdd89c
sakitkepala <andikapriyotama@gmail.com>
2019-07-14 21:32:16 +0700

    Cantumkan view di list data

--- custom-addons/todo_app/__manifest__.py
+++ custom-addons/todo_app/__manifest__.py
@@ -3,6 +3,11 @@
   'description': 'Manage personal to-do tasks.',
   'author': 'Daniel Ries, Andika Priyotama',
   'version': '12.0.1.0',
-  'depends': ['base'],
   'application': True,
+
+  'depends': ['base'],
+  'data': [
+    'security/ir.model.access.csv',
+    'views/todo_menu.xml',
+  ],
 }

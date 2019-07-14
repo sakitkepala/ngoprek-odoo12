9d5db1ed15e28046efb24ac5ff95298ee42096ab
sakitkepala <andikapriyotama@gmail.com>
2019-07-14 19:45:32 +0700

    Commit awal modul app Todo, isinya file init & manifest

---
+++ custom-addons/todo_app/__manifest__.py
@@ -0,0 +1,8 @@
+{
+  'name': 'To-Do Application',
+  'description': 'Manage personal to-do tasks.',
+  'author': 'Daniel Ries, Andika Priyotama',
+  'version': '12.0.1.0'
+  'depends': ['base'],
+  'application': True,
+}
